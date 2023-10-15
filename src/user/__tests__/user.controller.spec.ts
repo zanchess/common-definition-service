08a7c3c33fc950eb512faf36f65586728b76c538
Aliaksandr_Lashkevich <Aliaksandr.Lashkevich@coxautoinc.com>
2023-10-15 22:57:17 +0300

    feat(init): test cases name fix

--- src/user/__tests__/user.controller.spec.ts
+++ src/user/__tests__/user.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
 
-describe('AuthorizationController', () => {
+describe('UserController', () => {
     let controller: UserController;
 
     beforeEach(async () => {
