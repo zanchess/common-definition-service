adecb61b1a51103ddc969814ddd89e4e55b2d6c3
Aliaksandr_Lashkevich <Aliaksandr.Lashkevich@coxautoinc.com>
2023-10-23 00:31:18 +0300

    feat: fix test cases

--- src/user/__tests__/user.service.spec.ts
+++ src/user/__tests__/user.service.spec.ts
@@ -1,12 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '../user.service';
+import { UserController } from '../user.controller';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('UserService', () => {
     let service: UserService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [UserService]
+            controllers: [UserController],
+            providers: [
+                UserService,
+                {
+                    provide: getModelToken('User'), // Use the schema name passed to getModelToken()
+                    useValue: {} // Add a mock model or any specific methods if needed
+                }
+            ]
         }).compile();
 
         service = module.get<UserService>(UserService);
